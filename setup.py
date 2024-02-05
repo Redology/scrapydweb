b91d90edf2ffe15c50816bc9b7bf9802477072a6
Evan Collier <collier_e@yahoo.com>
2024-02-05 13:11:37 -0700

    remove requires versions and force 3.11

--- setup.py
+++ setup.py
@@ -31,29 +31,29 @@ setup(
     packages=find_packages(exclude=("tests", )),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <=3.9, <=3.11",
+    python_requires=">=2.7, ==3.11",
     install_requires=[
-        "APScheduler==3.5.3",  # Aug 15, 2018
-        "click==7.0",  # Sep 26, 2018
-        "colorama==0.4.0",  # Oct 10, 2018
-        "Flask==1.0.2",  # May 2, 2018
-        "Flask-Compress==1.4.0",  # Jan 5, 2017
-        "Flask-SQLAlchemy==2.4.0",  # Apr 25, 2019
-        "idna==2.7",  # Jun 11, 2018    
-        "itsdangerous==1.1.0",  # Oct 27, 2018
-        "Jinja2==2.10",  # Nov 9, 2017
-        "logparser==0.8.2",
-        "MarkupSafe==1.1.1",  # Feb 24, 2019
-        "pexpect==4.7.0",  # Apr 7, 2019
-        "ptyprocess==0.6.0",  # Jun 22, 2018
-        "pytz==2018.9",  # Jan 7, 2019
-        "requests>=2.21.0",  # Dec 10, 2018
-        "setuptools>=40.6.3",  # Dec 11, 2018
-        "six==1.12.0",  # Dec 10, 2018
-        "SQLAlchemy==1.3.24",  # Mar 31, 2021
-        "tzlocal==1.5.1",  # Dec 1, 2017
-        "w3lib==1.19.0",  # Jan 25, 2018
-        "Werkzeug==0.14.1",  # Jan 1, 2018
+        "APScheduler",  # Aug 15, 2018
+        "click",  # Sep 26, 2018
+        "colorama",  # Oct 10, 2018
+        "Flask",  # May 2, 2018
+        "Flask-Compress",  # Jan 5, 2017
+        "Flask-SQLAlchemy",  # Apr 25, 2019
+        "idna",  # Jun 11, 2018    
+        "itsdangerous",  # Oct 27, 2018
+        "Jinja2",  # Nov 9, 2017
+        "logparser",
+        "MarkupSafe",  # Feb 24, 2019
+        "pexpect",  # Apr 7, 2019
+        "ptyprocess",  # Jun 22, 2018
+        "pytz",  # Jan 7, 2019
+        "requests",  # Dec 10, 2018
+        "setuptools",  # Dec 11, 2018
+        "six",  # Dec 10, 2018
+        "SQLAlchemy",  # Mar 31, 2021
+        "tzlocal",  # Dec 1, 2017
+        "w3lib",  # Jan 25, 2018
+        "Werkzeug",  # Jan 1, 2018
     ],
 
     entry_points={
